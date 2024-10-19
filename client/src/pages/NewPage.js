30297020d204d8f717a0e2dd05b556c790888382
rainof <13416303+rainof@users.noreply.github.com>
2024-10-19 22:33:29 +0700

    fix(frontend): resolve issue preventing clicks on other links after deleting the current link

--- client/src/pages/NewPage.js
+++ client/src/pages/NewPage.js
@@ -1,11 +1,31 @@
 import React from "react";
-import TopBar from "../components/NavBar";
+import NavBar from "../components/NavBar";
 import InputField from "../components/InputField";
 
-function NewPage({ chats, setClickAdd, handleSend }) {
+function NewPage({
+  chats,
+  setChats,
+  activeChatId,
+  setActiveChatId,
+  setClickAdd,
+  handleSend,
+  updateResponse,
+  getChatResponse,
+  fetchChatById,
+  setIsDelete,
+}) {
   return (
     <>
-      <TopBar chats={chats} setClickAdd={setClickAdd} />
+      <NavBar
+        chats={chats}
+        setChats={setChats}
+        activeChatId={activeChatId}
+        setActiveChatId={setActiveChatId}
+        setClickAdd={setClickAdd}
+        getChatResponse={getChatResponse}
+        fetchChatById={fetchChatById}
+        setIsDelete={setIsDelete}
+      />
       {/* Blank content for a new page */}
       <div className="content-container">
         <p>No chats yet. Start a new conversation!</p>
